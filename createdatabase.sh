a512e6a4248b6c119bd0753394692af043622171
Ayormitan <Kolawoleabaz97@gmail.com>
2025-03-25 04:58:05 -0700

    Delete database

--- createdatabase.sh
+++ createdatabase.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 mysql -u root -p'Paasword' -e "CREATE DATABASE NewDatabase;" 2>/dev/null
+
---
+++ deletedatabse.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+mysql -u root -p"Paasword" -e "DROP DATABASE IF EXISTS newdatabase;"
