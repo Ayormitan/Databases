aff271cfb0abe5ea79c0975974d237ff9b90b5d6
Ayormitan <Kolawoleabaz97@gmail.com>
2025-04-03 13:56:16 -0700

    Listed all shows and genres using inner join

---
+++ MoreDatabses/list_allshows.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+mysql -u root -p'Paasword' -e "
+USE hbtn_0d_tvshows
+SELECT tv_shows.title, tv_genres.name AS name
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY tv_shows.title asc;
+"
