fd93ea91bbfa7e36e9fd9d13e710faa5af95a9b3
Ayormitan <Kolawoleabaz97@gmail.com>
2025-04-03 08:50:13 -0700

    Returns show with NULL genre

---
+++ MoreDatabses/No_genre.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+mysql -u root -p'Paasword' -e "
+USE hbtn_0d_tvshows
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;" 2>/dev/null
